3bfdf94cc8b4c0e6aab43f4f9824f45e0ad1aec1
Bahadır Bör <bahadir.bor.01@gmail.com>
2025-03-05 22:12:54 +0300

    Created channel adding func

--- source/database.py
+++ source/database.py
@@ -7,14 +7,38 @@ load_dotenv(dotenv_path="../config/.env")
 
 DATABASE_PATH = os.getenv("YOUR_DATABASE_URL")
 
-conn = sqlite3.connect(DATABASE_PATH)
-cursor = conn.cursor()
+def creating_database():
+    """Creating database.
+    You must execute this function in first run"""
+    conn = sqlite3.connect(DATABASE_PATH)
+    cursor = conn.cursor()
 
-with open("../data/database_schema.sql", "r") as sql_codes:
-    sql_script = sql_codes.read()
-    cursor.executescript(sql_script)
+    # Creating database tables
+    with open("../data/database_schema.sql", "r") as sql_codes:
+        sql_script = sql_codes.read()
+        cursor.executescript(sql_script)
 
-conn.commit()
-conn.close()
+    conn.commit()
+    conn.close()
 
-print(f"Database has created: {DATABASE_PATH}")
+    print("Database has created")
+
+def add_channel(channel_name,channel_id):
+    """Adding your channels"""
+    conn = sqlite3.connect(DATABASE_PATH)
+    cursor = conn.cursor()
+
+    try:
+        cursor.execute("""
+            INSERT OR IGNORE INTO channels (id, name)
+            VALUES (?,?)
+        """,(channel_id,channel_name))
+
+        conn.commit()
+    except Exception as e:
+        conn.rollback()
+        print(f"There is an error: {e}")
+    finally:
+        conn.close()
+
+    print("The channel has added!")
