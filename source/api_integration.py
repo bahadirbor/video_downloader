c580cf806bed4c66f48b9565aa7e5ec239311014
Bahadır Bör <bahadir.bor.01@gmail.com>
2025-03-09 00:40:41 +0300

    Functions updated for video scraping in main. Some bugs fixed

--- source/api_integration.py
+++ source/api_integration.py
@@ -3,49 +3,59 @@ import os
 import sqlite3
 import googleapiclient.discovery
 
-load_dotenv(dotenv_path="config/.env")
+load_dotenv(dotenv_path="../config/.env")
 
-YOUTUBE_API = os.getenv("YOUR_YOUTUBE_API_KEY")
-DATABASE_PATH = os.getenv("YOUR_DATABASE_URL")
+"""Switch type to str for avoid errors"""
+YOUTUBE_API = str(os.getenv("YOUR_YOUTUBE_API_KEY"))
+DATABASE_PATH = str(os.getenv("YOUR_DATABASE_URL"))
 
 def check_api_key():
-    """Test our API key work"""
+    """Test our API key work(optional)"""
     try:
-        youtube = googleapiclient.discovery.build("youtube", "v3", developerKey=YOUTUBE_API)
-        request = youtube.channels().list(part="id", id="insert_any_youtube_channel_id")
+        youtube_test = googleapiclient.discovery.build("youtube", "v3", developerKey=YOUTUBE_API)
+        """If you wanna test your api you should fill id parameter in list(id=)"""
+        request = youtube_test.channels().list(part="id", id="insert_any_youtube_channel_id")
         response = request.execute()
         if "items" in response:
-            print("API key çalışıyor")
+            print("API key works")
         else:
-            print("API key üzerinde bir sorun var")
+            print("There is an issue at API key")
     except Exception as e:
-        print("Bazı hatalar var: ", e)
+        print("There is an error: ", e)
 
-youtube = googleapiclient.discovery.build("youtube","v3",developerKey=YOUTUBE_API)
 
-def get_channels():
+def get_channels(path):
     """Get channels from database"""
-    conn = sqlite3.connect(DATABASE_PATH)
+    conn = sqlite3.connect(path)
     cursor = conn.cursor()
     cursor.execute("SELECT id FROM channels")
     channels = [row[0] for row in cursor.fetchall()]
     conn.close()
     return channels
 
-def get_latest_videos(channel_id):
+def get_channel_name(channel_id,path):
+    conn = sqlite3.connect(path)
+    cursor = conn.cursor()
+    cursor.execute("SELECT name FROM channels WHERE id = ?",(channel_id,))
+    name = [row[0] for row in cursor.fetchall()]
+    conn.close()
+    return name[0]
+
+def get_latest_videos(channel_id, api):
     """Get last videos from our channel list"""
+    youtube = googleapiclient.discovery.build("youtube", "v3", developerKey=api)
     request = youtube.search().list(
         part="id,snippet",
         channelId = channel_id,
         order = "date",
-        maxResults = 3
+        maxResults = 3,
     )
     response = request.execute()
     return response.get("items",[])
 
-def save_to_database(videos, channel_id):
+def save_to_database(videos, channel_id, path):
     """Save video information to database"""
-    conn = sqlite3.connect(DATABASE_PATH)
+    conn = sqlite3.connect(path)
     cursor = conn.cursor()
 
     for video in videos:
@@ -55,7 +65,7 @@ def save_to_database(videos, channel_id):
 
         cursor.execute("""
             INSERT OR IGNORE INTO videos (id,channel_id,title,published_at)
-            VALUES (?,?,?,?,0)
+            VALUES (?,?,?,?)
         """,(video_id,channel_id,title,published_at))
 
     conn.commit()
--- source/database.py
+++ source/database.py
@@ -5,12 +5,13 @@ from dotenv import load_dotenv
 # importing env file
 load_dotenv(dotenv_path="../config/.env")
 
-DATABASE_PATH = os.getenv("YOUR_DATABASE_URL")
+DATABASE_PATH = str(os.getenv("YOUR_DATABASE_URL"))
 
-def creating_database():
+
+def creating_database(path):
     """Creating database.
     You must execute this function in first run"""
-    conn = sqlite3.connect(DATABASE_PATH)
+    conn = sqlite3.connect(path)
     cursor = conn.cursor()
 
     # Creating database tables
@@ -23,22 +24,28 @@ def creating_database():
 
     print("Database has created")
 
-def add_channel(channel_name,channel_id):
+def add_channel(path):
     """Adding your channels"""
-    conn = sqlite3.connect(DATABASE_PATH)
+    channel_id = input("Insert channel id: ")
+    channel_name = input("Insert channel name: ")
+    conn = sqlite3.connect(path)
     cursor = conn.cursor()
 
     try:
         cursor.execute("""
             INSERT OR IGNORE INTO channels (id, name)
             VALUES (?,?)
-        """,(channel_id,channel_name))
-
+        """,(str(channel_id),str(channel_name)))
         conn.commit()
+    
     except Exception as e:
         conn.rollback()
         print(f"There is an error: {e}")
+        
     finally:
         conn.close()
 
     print("The channel has added!")
+
+
+creating_database(DATABASE_PATH)
