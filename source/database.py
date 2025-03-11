22459725d8c5a6436361aac2cbef0e2a36e1ffde
Bahadır Bör <bahadir.bor.01@gmail.com>
2025-03-11 12:11:27 +0300

    Add database crud functions

--- source/database.py
+++ source/database.py
@@ -25,6 +25,7 @@ def creating_database(path):
 
     print("Database has created")
 
+
 def add_channel(path):
     """Adding your channels"""
     channel_id = input("Insert channel id: ")
@@ -49,6 +50,24 @@ def add_channel(path):
     print("The channel has added!")
 
 
+def delete_channel(database_path):
+    channel_name = input("Enter channel name for delete: ")
+    conn = sqlite3.connect(database_path)
+    cursor = conn.cursor()
+
+    try:
+        cursor.execute("""
+            DELETE FROM channels WHERE name = (?) 
+        """, (channel_name,))
+        conn.commit()
+
+    except Exception as e:
+        print("There is an error: " + str(e))
+
+    finally:
+        conn.close()
+
+
 def get_channel_ids(database_path):
     """Getting channel id infos from database"""
     try:
@@ -61,3 +80,15 @@ def get_channel_ids(database_path):
         return channel_ids
     except Exception as e:
         print("There is an exception: " + str(e))
+
+
+def change_download_status(video_id, database_path):
+    conn = sqlite3.connect(database_path)
+    cursor = conn.cursor()
+
+    cursor.execute("""
+        UPDATE videos SET download = 1 WHERE id = (?)
+    """,(video_id,))
+
+    conn.commit()
+    conn.close()
